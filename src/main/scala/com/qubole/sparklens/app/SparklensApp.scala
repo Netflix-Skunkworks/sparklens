ba75aa4d07e82d53f4e655cf7eeca1b5c8911cfb
Dhruv Pratap <dpratap@netflix.com>
2024-02-27 17:24:29 -0600

    NETFLIX-BUILD: Expose Sparklens app as a service to run purely using bdp-s3fs and outside a spark context.

---
+++ src/main/scala/com/qubole/sparklens/app/SparklensApp.scala
@@ -0,0 +1,32 @@
+package com.qubole.sparklens.app
+
+import org.apache.spark.SparkConf
+import org.apache.spark.deploy.history.EventHistoryReplayer
+
+import java.io.ByteArrayOutputStream
+
+class SparklensApp {
+
+  def analyzeFromEventHistory(sparkConf: SparkConf, appId: String): String = {
+    analyzeFromEventHistory(sparkConf, appId, None)
+  }
+
+  def analyzeFromEventHistory(sparkConf: SparkConf, appId: String, attemptId: Option[String]): String = {
+
+    val outCapture = new ByteArrayOutputStream()
+    val errCapture = new ByteArrayOutputStream()
+
+    Console.withOut(outCapture) {
+      Console.withErr(errCapture) {
+        new EventHistoryReplayer(sparkConf, appId, attemptId)
+      }
+    }
+
+    if (outCapture.toString.nonEmpty) // "yes"
+      outCapture.toString
+    else if (errCapture.toString.nonEmpty) // "NO!"
+      errCapture.toString
+    else
+      throw new IllegalArgumentException(s"Failed to analyze event history for appId: $appId and attemptId: $attemptId")
+  }
+}
